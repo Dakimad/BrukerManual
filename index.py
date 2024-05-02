fa2677b2d808a6337fe9fabe80e60c23db0e5762
Daniel Kirkenaer Mathisen <danielkmathisen@gmail.com>
2024-05-02 13:14:46 +0200

    startet med koble python koden min opp mot serveren min

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
-import tkinter 
 import pymysql.cursors
-import random
+
 # Oppretter en tilkobling til databasen
 connection = pymysql.connect(host='172.20.128.84', 
                             user='Daniel',         
@@ -8,3 +7,10 @@ connection = pymysql.connect(host='172.20.128.84',
                             database='Users',     
                             charset='utf8mb4',      
                             cursorclass=pymysql.cursors.DictCursor)
+
+def insert_user(username, bruker_id, passord):
+    # Setter inn brukernavn, brukerID og passord i databasen
+    with connection.cursor() as cursor:
+        cursor.execute("INSERT INTO Brukere (Navn, BrukerID, Passord) VALUES (%s, %s, %s)", (username, bruker_id, passord))
+    connection.commit()
+ 
